b58512040d3e50c336d7ff441e8c540e9ea69791
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-04 15:30:14 +0300

    Lift state up to app component, pass state and setter function as props to child components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,30 @@
+import { useEffect, useState } from "react";
 import Background from "./Background";
 import Container from "./Container";
 import Footer from "./Footer";
 import Header from "./Header";
 
 function App() {
+  const [searchText, setSearchText] = useState("");
+  const [jobItems, setJobItems] = useState([]);
+
+  useEffect(() => {
+    if (!searchText) return;
+
+    const fetchData = async () => {
+      const response = await fetch(
+        `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
+      );
+      const data = await response.json();
+      setJobItems(data.jobItems);
+    };
+    fetchData();
+  }, [searchText]);
   return (
     <>
       <Background />
-      <Header />
-      <Container />
+      <Header searchText={searchText} setSearchText={setSearchText} />
+      <Container jobItems={jobItems} />
       <Footer />
     </>
   );
--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,21 +1,4 @@
-import { useEffect, useState } from "react";
-
-export default function SearchForm() {
-  const [searchText, setSearchText] = useState("");
-  const [jobItems, setJobItems] = useState([]);
-  useEffect(() => {
-    if (!searchText) return;
-
-    const fetchData = async () => {
-      const response = await fetch(
-        `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
-      );
-      const data = await response.json();
-      setJobItems(data.jobItems);
-    };
-    fetchData();
-  }, [searchText]);
-
+export default function SearchForm({ searchText, setSearchText }) {
   return (
     <form
       onSubmit={(e) => {
