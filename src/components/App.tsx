9d2898934e1c029649ebf752f0bc0318561ec37a
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-04 17:31:39 +0300

    Add a loading state(spinner) while fetching data

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -16,15 +16,18 @@ import JobList from "./JobList";
 function App() {
   const [searchText, setSearchText] = useState("");
   const [jobItems, setJobItems] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     if (!searchText) return;
 
     const fetchData = async () => {
+      setIsLoading(true);
+
       const response = await fetch(
         `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
       );
       const data = await response.json();
+      setIsLoading(false);
       setJobItems(data.jobItems);
     };
     fetchData();
@@ -47,7 +50,7 @@ function App() {
             <SortingControls />
           </SidebarTop>
 
-          <JobList jobItems={jobItems} />
+          <JobList jobItems={jobItems} isLoading={isLoading} />
           <PaginationControls />
         </Sidebar>
         <JobItemContent />
