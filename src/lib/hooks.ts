e14ee723e30cc9fd53ef12a2769c6632457bfb2b
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-05 02:17:50 +0300

    Add type state for expanded job detaisl

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { JobItem } from "./types";
+import { JobItem, JobItemExpanded } from "./types";
 import { BASE_API_URL } from "./constants";
 
 export function useActiveId() {
@@ -22,7 +22,7 @@ export function useActiveId() {
 }
 
 export function useJobItem(id: number | null) {
-  const [jobItem, setJobItem] = useState(null);
+  const [jobItem, setJobItem] = useState<JobItemExpanded | null>(null);
 
   useEffect(() => {
     if (!id) return;
