0178224df1fdf7c43c26424f8abf507b7cee3457
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-05 12:28:25 +0300

    Add utility function to handle unknown errors

---
+++ src/lib/utils.ts
@@ -0,0 +1,16 @@
+import toast from "react-hot-toast";
+
+export const handleError = (error: unknown) => {
+  let message;
+
+  if (error instanceof Error) {
+    message = error.message;
+  } else if (typeof error === "string") {
+    message = error;
+  } else {
+    message = "An error occurred";
+  }
+
+  toast.error(message);
+  console.log(error);
+};
