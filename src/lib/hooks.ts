4eb9fa0ee0e33a6bb2e00457ac7ec5667ac220a8
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-05 10:22:46 +0300

    Refactor data fetching to React-query

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,42 +1,26 @@
 import { useEffect, useState } from "react";
-import { JobItem, JobItemExpanded } from "./types";
+import { JobItem } from "./types";
 import { BASE_API_URL } from "./constants";
-
-export function useActiveId() {
-  const [activeId, setActiveId] = useState<number | null>(null);
-
-  useEffect(() => {
-    const handleHashChange = () => {
-      const id = +window.location.hash.slice(1);
-      setActiveId(id);
-    };
-    handleHashChange();
-
-    window.addEventListener("hashchange", handleHashChange);
-
-    return () => {
-      window.removeEventListener("hashchange", handleHashChange);
-    };
-  }, []);
-  return activeId;
-}
+import { useQuery } from "@tanstack/react-query";
 
 export function useJobItem(id: number | null) {
-  const [jobItem, setJobItem] = useState<JobItemExpanded | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
-  useEffect(() => {
-    if (!id) return;
-
-    const fetchData = async () => {
-      setIsLoading(true);
+  const { data, isLoading } = useQuery(
+    ["job-item", id],
+    async () => {
       const response = await fetch(`${BASE_API_URL}/${id}`);
       const data = await response.json();
-      setIsLoading(false);
-      setJobItem(data.jobItem);
-    };
-    fetchData();
-  }, [id]);
-
+      return data;
+    },
+    {
+      staleTime: 1000 * 60 * 60,
+      refetchOnWindowFocus: false,
+      retry: false,
+      enabled: Boolean(id),
+      onError: () => {},
+    }
+  );
+  console.log(data);
+  const jobItem = data.jobItem;
   return { jobItem, isLoading } as const;
 }
 
@@ -78,4 +62,23 @@ export function useDebounce<T>(value: T, delay = 500): T {
   }, [value, delay]);
 
   return debouncedvalue;
+}
+
+export function useActiveId() {
+  const [activeId, setActiveId] = useState<number | null>(null);
+
+  useEffect(() => {
+    const handleHashChange = () => {
+      const id = +window.location.hash.slice(1);
+      setActiveId(id);
+    };
+    handleHashChange();
+
+    window.addEventListener("hashchange", handleHashChange);
+
+    return () => {
+      window.removeEventListener("hashchange", handleHashChange);
+    };
+  }, []);
+  return activeId;
 }
