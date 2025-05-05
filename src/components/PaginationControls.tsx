8b8b9b9a5fc48cc0eba733a2c437ba9cfe47ce3a
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-05 13:31:30 +0300

    Create reusable pagination components and add types

--- src/components/PaginationControls.tsx
+++ src/components/PaginationControls.tsx
@@ -1,3 +1,60 @@
-export default function PaginationControls() {
-  return <section className="pagination"></section>;
+import { ArrowLeftIcon, ArrowRightIcon } from "@radix-ui/react-icons";
+
+type PaginationControlsProps = {
+  currentPage: number;
+  onClick: (direction: "next" | "previous") => void;
+};
+
+export default function PaginationControls({
+  currentPage,
+  onClick,
+}: PaginationControlsProps) {
+  return (
+    <section className="pagination">
+      {currentPage > 1 && (
+        <PaginationButton
+          direction="previous"
+          currentPage={currentPage}
+          onClick={() => onClick("previous")}
+        />
+      )}
+
+      <PaginationButton
+        direction="next"
+        currentPage={currentPage}
+        onClick={() => onClick("next")}
+      />
+    </section>
+  );
+}
+type PaginationButtonProps = {
+  direction: "next" | "previous";
+  currentPage: number;
+  onClick: () => void;
+};
+
+function PaginationButton({
+  direction,
+  currentPage,
+  onClick,
+}: PaginationButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className={`pagination__button pagination__button--${direction}`}
+    >
+      {direction === "previous" && (
+        <>
+          <ArrowLeftIcon />
+          Page {currentPage - 1}
+        </>
+      )}
+      {direction === "next" && (
+        <>
+          Page {currentPage + 1}
+          <ArrowRightIcon />
+        </>
+      )}
+    </button>
+  );
 }
