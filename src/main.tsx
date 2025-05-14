d6821dddf26d3ee7b45e215c4f0db8f3e0660b17
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-14 12:35:04 +0300

    Wrap App component with JobItems and searchText context provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,17 +5,23 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import BookmarksContextProvider from "./context/BookmarksContextProvider.tsx";
 import ActiveIdContextProvider from "./context/ActiveIdContextProvider.tsx";
+import SearchTextContextProvider from "./context/SearchTextContextProvider.tsx";
+import JobItemsContextProvider from "./context/JobItemsContextProvider.tsx";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BookmarksContextProvider>
-        <ActiveIdContextProvider>
-          <App />
-        </ActiveIdContextProvider>
-      </BookmarksContextProvider>
+      <SearchTextContextProvider>
+        <BookmarksContextProvider>
+          <ActiveIdContextProvider>
+            <JobItemsContextProvider>
+              <App />
+            </JobItemsContextProvider>
+          </ActiveIdContextProvider>
+        </BookmarksContextProvider>
+      </SearchTextContextProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
