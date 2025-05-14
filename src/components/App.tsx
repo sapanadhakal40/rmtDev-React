a36a9c1cbddd96f528efc6a851a73980f002bbd0
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-14 12:37:44 +0300

    Remove prop drilling in app component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import Background from "./Background";
 import Container from "./Container";
 import Footer from "./Footer";
@@ -11,50 +10,15 @@ import Sidebar, { SidebarTop } from "./Sidebar";
 import ResultsCount from "./ResultsCount";
 import SortingControls from "./SortingControls";
 import PaginationControls from "./PaginationControls";
-import JobList from "./JobList";
-import { useDebounce, useSearchQuery } from "../lib/hooks";
 import { Toaster } from "react-hot-toast";
-import { RESULTS_PER_PAGE } from "../lib/constants";
-import { PageDirection, SortBy } from "../lib/types";
+import JobListSearch from "./JobListSearch";
 
 function App() {
   //state
-  const [searchText, setSearchText] = useState("");
-  const debouncedSearchText = useDebounce(searchText, 250);
+  // const [searchText, setSearchText] = useState("");
+  // const debouncedSearchText = useDebounce(searchText, 250);
 
   // const { jobItemsSliced: jobItems, isLoading } = useJobItems(searchText);
-  const { jobItems, isLoading } = useSearchQuery(debouncedSearchText);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [sortBy, setSortBy] = useState<SortBy>("relevant");
-
-  //derived/ computed state
-  const totalNumberOfResults = jobItems?.length || 0;
-  const totalNumberOfPages = totalNumberOfResults / RESULTS_PER_PAGE;
-  const jobItemSorted = [...(jobItems || [])].sort((a, b) => {
-    if (sortBy === "relevant") {
-      return b.relevanceScore - a.relevanceScore; //b sorted higher iff it is positive
-    } else {
-      return a.daysAgo - b.daysAgo;
-    }
-  });
-  const jobItemsSortedAndSliced = jobItemSorted.slice(
-    currentPage * RESULTS_PER_PAGE - RESULTS_PER_PAGE,
-    currentPage * RESULTS_PER_PAGE
-  );
-
-  //event handlers
-
-  const handleChangePage = (direction: PageDirection) => {
-    if (direction === "next") {
-      setCurrentPage((prev) => prev + 1);
-    } else if (direction === "previous") {
-      setCurrentPage((prev) => prev - 1);
-    }
-  };
-  const handleChangeSortBy = (newSortBy: SortBy) => {
-    setCurrentPage(1);
-    setSortBy(newSortBy);
-  };
 
   // const jobItem = useActiveJobItem();
   return (
@@ -66,21 +30,17 @@ function App() {
           <BookmarksButton />
         </HeaderTop>
 
-        <SearchForm searchText={searchText} setSearchText={setSearchText} />
+        <SearchForm />
       </Header>
       <Container>
         <Sidebar>
           <SidebarTop>
-            <ResultsCount totalNumberOfResults={totalNumberOfResults} />
-            <SortingControls sortBy={sortBy} onClick={handleChangeSortBy} />
+            <ResultsCount />
+            <SortingControls />
           </SidebarTop>
+          <JobListSearch />
 
-          <JobList jobItems={jobItemsSortedAndSliced} isLoading={isLoading} />
-          <PaginationControls
-            currentPage={currentPage}
-            onClick={handleChangePage}
-            totalNumberOfPages={totalNumberOfPages}
-          />
+          <PaginationControls />
         </Sidebar>
         <JobItemContent />
       </Container>
